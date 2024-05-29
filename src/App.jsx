c63c8438295108e158150419728364ff56ae4b33
Yejin Choi <llhdf123@naver.com>
2024-05-29 19:54:10 +0900

    todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,87 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const plusHandler = () => {
-    setCount(count + 1);
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "잠자기",
+      isDone: true,
+    },
+    {
+      id: 2,
+      text: "일찍 일어나기",
+      isDone: false,
+    },
+  ]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!input.trim()) {
+      alert("할 일을 입력해주세요");
+      return;
+    }
+    const newTodo = {
+      id: uuidv4(),
+      text: input,
+      isDone: false,
+    };
+    setTodos([newTodo, ...todos]);
+    setInput("");
   };
-  const minusHandler = () => {
-    setCount(count - 1);
+
+  const deleteHandler = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const isDoneChangeHandler = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
   };
 
   return (
     <div>
-      Count: {count >= 0 ? count : setCount(0)}
-      <button onClick={plusHandler}>+</button>
-      <button onClick={minusHandler}>-</button>
+      <h1>할 일 목록</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          placeholder="할 일을 추가하세요"
+        />
+        <button type="submit">추가</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{ textDecoration: todo.isDone ? "line-through" : "none" }}
+          >
+            {todo.text}
+            <button onClick={() => isDoneChangeHandler(todo.id)}>
+              {todo.isDone ? "취소" : "완료"}
+            </button>
+            <button onClick={() => deleteHandler(todo.id)}>삭제</button>
+          </li>
+        ))}
+        {/* <li style={{ textDecoration: "line-through" }}>
+          잠자기
+          <button>취소</button>
+          <button>삭제</button>
+        </li>
+        <li>
+          일찍 일어나기
+          <button>완료</button>
+          <button>삭제</button>
+        </li> */}
+      </ul>
     </div>
   );
 }
 
 export default App;
-
-// import { useState } from "react";
-// function App() {
-//   const [isDark, setIsDark] = useState(false);
-//   const changeMode = () => {
-//     setIsDark(!isDark);
-//   };
-//   return (
-//     <div>
-//       {isDark ? "다크모드" : "화이트모드"}
-//       <button onClick={changeMode}>change</button>
-//     </div>
-//   );
-// }
-
-// export default App;
